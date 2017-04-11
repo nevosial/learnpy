dd7b2b0ef3a2e27cc46e51030c623a28dfea3c88
nevosial <nevosial@gmail.com>
2017-04-11 23:29:57 +0530

    added file code

--- simpleparse.py
+++ simpleparse.py
@@ -6,6 +6,30 @@ import datetime
 from datetime import date, time, timedelta
 import time
 
+# Open a file for writing and create it if it doesn't exist
+  f = open("textfile.txt","w+")
+
+  # Open the file for appending text to the end
+#  f = open("textfile.txt","a+")
+
+  # write some lines of data to the file
+  for i in range(10):
+    f.write("This is line %d\r\n" % (i+1))
+
+  # close the file when done
+  f.close()
+
+  # Open the file back up and read the contents
+  f = open("textfile.txt","r")
+  if f.mode == 'r': # check to make sure that the file was opened
+    # use the read() function to read the entire file
+    # contents = f.read()
+    # print (contents)
+
+    fl = f.readlines() # readlines reads the individual lines into a list
+    for x in fl:
+      print (x)
+      
 
   # Print the name of the OS
   print (os.name)
