1e8c799422170624a90cf1bb41b8cea6ab2d6cc4
nevosial <nevosial@gmail.com>
2017-04-15 00:19:23 +0530

    Added new try except

--- Regex.py
+++ Regex.py
@@ -14,11 +14,25 @@ re.IGNORECASE
 
 # Exception Handling in Python
 def main():
+    #example of a IO error
     try:
         fh = open('lines.txt')
         for line in fh: print(line , end='')
     except IOError as e:
         print('Could not open file:' , e)
+
+    #example for a ValueError
+    try:
+        print('Enter a numerical value')
+        numb = int(input())
+        if numb % 2 == 0:
+            print("Number is an even number")
+        else:
+            print('Number is an odd number')
+
+
+    except ValueError as e:
+        print('Please enter an integer value. System error :{}'.format(e))
 
 if __name__ == "__main__": main()
 
