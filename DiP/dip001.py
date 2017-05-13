efd7baa2d13b9cbb3c83288982cec64acc9a0160
nevosial <nevosial@gmail.com>
2017-05-13 18:44:38 +0530

    updated

--- DiP/dip001.py
+++ DiP/dip001.py
@@ -30,3 +30,5 @@ def approximate_size(size , a_kilobyte_is_1024_bytes= True):
 if __name__ == '__main__':
     print(approximate_size(1000000000000, False))
     print(approximate_size(1000000000000))
+
+#Go to a drive and run this file.
