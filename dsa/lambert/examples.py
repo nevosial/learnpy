d6dacedb561300dcd9a111c45348e34342be3344
Neville Mascarenhas <nevosial@gmail.com>
2017-06-25 21:37:41 +0530

    Update examples.py

--- dsa/lambert/examples.py
+++ dsa/lambert/examples.py
@@ -8,3 +8,15 @@
     name = 'three'
     print(choices.get(name, 'default'))
     #will print default if input is not in the dict.
+    
+    
+#Function with variable number of arguments
+#Here args is the list that is passed to the function.
+#sometimes called as unpacker.
+def multi(*args):
+    result = 0;
+    for i in args:
+        result = result + i;
+    return result;
+
+#print(multi(1, 2, 3))
