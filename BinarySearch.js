3d1a03d863b5d315442577689b002f300fc35ca6
Neville Mascarenhas <nevosial@gmail.com>
2017-06-29 14:29:13 +0530

    Create BinarySearch.js

---
+++ BinarySearch.js
@@ -0,0 +1,24 @@
+//Binary Search
+
+arr = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
+
+
+function bSearch(arr , target){
+	len = arr.length;
+	end = len - 1;
+	start = 0;
+	found ;
+	
+	while(start <= end){
+		mid = Math.floor((start+end)/2);
+		found = arr[mid];
+		if(target > found){
+			start = mid + 1;
+		} else if(target < found){
+			end = mid - 1;
+			
+		} else {
+			return mid;
+		}
+	}
+}
