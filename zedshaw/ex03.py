2c21ae4f6b246ae377e6eee345f551da04060e35
nevosial <nevosial@gmail.com>
2018-06-11 14:07:05 -0700

    numeric operations

---
+++ zedshaw/ex03.py
@@ -0,0 +1,20 @@
+# Math and numbers
+print(3+2)
+print(21/3)
+print(5-0)
+print(99090*1)
+
+print(2/1000)
+print(22/7)
+
+print(25%2)
+print(25%10)
+print(25%11)
+print(25%12)
+print(25%13)
+
+print(25 > 12)
+print(25 < 12)
+print(25 >= 25)
+print(25 <= 26)
+print(25 == 12)
