411c86c21980e66821e349b95b2a468f9ef6b665
nevosial <nevosial@gmail.com>
2018-06-11 15:53:23 -0700

    formatting string

---
+++ zedshaw/ex05.py
@@ -0,0 +1,12 @@
+#String formats and variables inside strings.
+print("===Employee Details===")
+e_name = "nev"
+e_age = 20
+e_dept = "Research Centre"
+e_state = 'CA'
+e_weight = 190 #lbs
+
+#Notice the way f {} is used.
+print(f"Employe name:{e_name}")
+print(f"Employee age:{e_age}")
+print(f"Employee dept:{e_dept}\nEmployee state:{e_state}")
