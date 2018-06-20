750a3bbfe61a0860467d63c08a66ead450b2cd1e
nevosial <nevosial@gmail.com>
2018-06-19 19:29:52 -0700

    argv and unpacker

---
+++ zedshaw/ex13.py
@@ -0,0 +1,10 @@
+#Unpacker and argv
+from sys import argv
+
+#unpack the args
+script, first, second, third = argv
+
+print("The script is called ",script)
+print("The first arg is ", first)
+print("The second arg is ", second)
+print("The third arg is ", third)
