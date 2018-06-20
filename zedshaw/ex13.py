e7a2cf4fd7f62dff9df6e1bf90aeeb2e6fe8c9ce
nevosial <nevosial@gmail.com>
2018-06-19 19:36:36 -0700

    argv, unpack, input()

--- zedshaw/ex13.py
+++ zedshaw/ex13.py
@@ -1,10 +1,15 @@
 #Unpacker and argv
 from sys import argv
 
+
+
 #unpack the args
-script, first, second, third = argv
+script, first, second, third, fourth = argv
+
+fourth = input("Enter your arg")
 
 print("The script is called ",script)
 print("The first arg is ", first)
 print("The second arg is ", second)
 print("The third arg is ", third)
+print("This is from user: ",fourth)
