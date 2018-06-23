590bb773a7a16eb9051aa74891e58795d68947cc
nevosial <nevosial@gmail.com>
2018-06-23 16:51:28 -0700

    dicts, input

--- refresh/diction.py
+++ refresh/diction.py
@@ -8,4 +8,21 @@ print(rollbook.items())
 
 print("Nev number: ",rollbook.get("nev"))
 print(rollbook)
-print(" # belongs to ", rollbook.get(47))
+print(" # belongs to ", rollbook.get("rock", "VIP"))
+
+l = ["my", "name", "is", "duder", "duder"]
+b = {0}
+for w in l:
+    #print(w)
+    b.add(w)  #add to a Set
+print(b)
+
+score = int(input("Enter score: "))
+if score >= 90:
+    print("A")
+elif score >= 75 and score <= 90:
+    print("B")
+elif score >= 60 and score <= 75:
+    print("C")
+else:
+    print("Did not qualify.")
